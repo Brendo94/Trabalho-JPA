1766680439d7fcec33933ac286d89aae9f0279c1
Brendo94 <brendocolaco@hotmail.com>
2014-10-16 14:51:01 -0300

    Criação do model de Departamento

---
+++ Lista3-jpa/src/br/ufc/persis/model/Departamento.java
@@ -0,0 +1,20 @@
+
+package br.ufc.persis.model;
+
+public class Departamento {
+	private String nome;
+	private int numero;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+}
