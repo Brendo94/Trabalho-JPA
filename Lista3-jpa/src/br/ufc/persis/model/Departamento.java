b10d6e555bb0100ce1e984542d152d4d945eb9a0
PC <PC@10.0.156.229>
2014-10-16 15:18:08 -0300

    Adição do construtor e do método toString na entidade departamento.

--- Lista3-jpa/src/br/ufc/persis/model/Departamento.java
+++ Lista3-jpa/src/br/ufc/persis/model/Departamento.java
@@ -5,6 +5,12 @@ public class Departamento {
 	private String nome;
 	private int numero;
 	
+	public Departamento(String nome, int numero) {
+		super();
+		this.nome = nome;
+		this.numero = numero;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
@@ -17,4 +23,11 @@ public class Departamento {
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
+	@Override
+	public String toString() {
+		return "Departamento [nome=" + nome + ", numero=" + numero + "]";
+	}
+	
+	
 }
